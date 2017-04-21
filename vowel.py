8321e9bb76fc208f6e379e780b5d036c61214b64
Andrew Rohde <andrewprohde@gmail.com>
2017-04-21 16:50:29 -0700

    Corrected functionality issues.
    Removed commented out section and reworked the output into a sentence that is readable.

--- vowel.py
+++ vowel.py
@@ -4,25 +4,12 @@ print (output)
 char_count = len(output)
 
 print ("There are " + str(char_count) + " characters in your sentence.")
+output = output.lower()
 
-#vowel_count = 0
-
-#def countvowels(output):
-    
-    #for char in ouput:
-        #if char == "a" or "e" or "i" or "o" or "u":
-            #vowel_count = vowel_count + 1
-    #return vowel_count
-
-#print ("There are " + str(vowel_count) + " vowels in your sentence.")
-vowel_a = output.ignorecase.count('a')
+vowel_a = output.count('a')
 vowel_e = output.count('e')
 vowel_i = output.count('i')
 vowel_o = output.count('o')
 vowel_u = output.count('u')
 
-print ("you used the letter A " + str(vowel_a) + " times")
-print ("you used the letter E " + str(vowel_e) + " times")
-print ("you used the letter I " + str(vowel_i) + " times")
-print ("you used the letter O " + str(vowel_o) + " times")
-print ("you used the letter U " + str(vowel_u) + " times")
+print ("you used the letter A " + str(vowel_a) + " times, E " + str(vowel_e) + " times, I " + str(vowel_i) + " times, O " + str(vowel_o) + " times, and U " + str(vowel_u) + " times")

88747754ab8d392724e02c6fa1f46703c44e282e
Andrew Rohde <andrewprohde@gmail.com>
2017-04-21 06:14:20 -0700

    Re-wrote the code to change functionality

--- vowel.py
+++ vowel.py
@@ -1,17 +1,28 @@
 #!/usr/bin/env python3
 output = input('Input Sentence:')
 print (output)
-char_count = output.count(output)
-str(char_count)
-print ("There are ") + char_count + (" characters in your sentence.")
+char_count = len(output)
 
-def countvowels(output):
-    vowel = 0
-    for char in ouput:
-        if char in "aeiouAEIOU":
-            vowel = vowel + 1
-    return vowel
-vowel_count = vowel
-str(vowel_count)
+print ("There are " + str(char_count) + " characters in your sentence.")
 
-print ("There are ") + vowel_count + (" vowels in your sentence.")
+#vowel_count = 0
+
+#def countvowels(output):
+    
+    #for char in ouput:
+        #if char == "a" or "e" or "i" or "o" or "u":
+            #vowel_count = vowel_count + 1
+    #return vowel_count
+
+#print ("There are " + str(vowel_count) + " vowels in your sentence.")
+vowel_a = output.ignorecase.count('a')
+vowel_e = output.count('e')
+vowel_i = output.count('i')
+vowel_o = output.count('o')
+vowel_u = output.count('u')
+
+print ("you used the letter A " + str(vowel_a) + " times")
+print ("you used the letter E " + str(vowel_e) + " times")
+print ("you used the letter I " + str(vowel_i) + " times")
+print ("you used the letter O " + str(vowel_o) + " times")
+print ("you used the letter U " + str(vowel_u) + " times")
